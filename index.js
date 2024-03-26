9f7d5c53197039c1a6a635639ae698f8be130af7
+ <+>
2024-03-26 16:39:01 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// code your solution here
+function superbowlWin(winResults) {
+    let winYear = winResults.find(result => result.result === "W");
+    return winYear ? winYear.year : undefined;
+}
